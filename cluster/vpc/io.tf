f0f9476d7a925581cc61ff1894763cb36dfe2048
Cemo <cemalettin.koc@gmail.com>
2017-05-26 09:43:45 +0300

    instead of data used null_resource for creating ip's

--- cluster/vpc/io.tf
+++ cluster/vpc/io.tf
@@ -3,5 +3,5 @@ variable "master-count" {}
 variable "subnet-ids-private" {}
 
 output "master-ips" {
-  value = "${join(",", data.template_file.master.*.rendered)}"
+  value = "${join(",", null_resource.master.*.triggers.ip)}"
 }
--- cluster/vpc/ip.tf
+++ cluster/vpc/ip.tf
@@ -2,7 +2,9 @@ data "aws_subnet" "private_subnet" {
   id = "${ element(split( ",",var.subnet-ids-private ), 0) }"
 }
 
-data "template_file" "master" {
+resource "null_resource" "master" {
   count    = "${ var.master-count }"
-  template = "${cidrhost(data.aws_subnet.private_subnet.cidr_block, var.master-cidr-offset + count.index )}"
+  triggers {
+    ip = "${cidrhost(data.aws_subnet.private_subnet.cidr_block, var.master-cidr-offset + count.index )}"
+  }
 }
